e2a89428e7eccf68bfe84aa5a9f7410d3c4031a0
swapper9 <32822455+swapper9@users.noreply.github.com>
2020-06-13 14:19:59 +0300

    compare 2 files
    show diff only if line absent in second file or changed despite it's location in file

--- file_line_compare.py
+++ file_line_compare.py
@@ -1,12 +1,18 @@
 import sys
 
 
-def compareFiles(p1, p2):
-    with open(p1) as f1, open(p2) as f2:
-        for line1, line2 in zip(f1, f2):
-            if line1 != line2:
-                print(p1 + ': ' + line1 + p2 + ': ' + line2)
+def read_file(file):
+    lines = [line.rstrip('\n') for line in open(file)]
+    return lines
 
 
 if len(sys.argv) == 3:
-    compareFiles(sys.argv[1], sys.argv[2])
+    lines1 = read_file(sys.argv[1])
+    lines2 = read_file(sys.argv[2])
+    for i in range(len(lines1)):
+        diff = True
+        for j in range(len(lines2)):
+            if lines1[i] == lines2[j]:
+                diff = False
+        if diff:
+            print(sys.argv[1] + ',#' + str(i) + ': ' + lines1[i] + '\n' + sys.argv[2] + ',#' + str(i) + ': ' + lines2[i] + '\n')

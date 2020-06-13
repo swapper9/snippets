d468d1bce7ef2e2332f286870dd10f75e4e891c3
swapper9 <32822455+swapper9@users.noreply.github.com>
2020-06-13 14:27:47 +0300

    compare from 2 sides

--- file_line_compare.py
+++ file_line_compare.py
@@ -6,13 +6,20 @@ def read_file(file):
     return lines
 
 
-if len(sys.argv) == 3:
-    lines1 = read_file(sys.argv[1])
-    lines2 = read_file(sys.argv[2])
+def compare(f1, f2):
+    lines1 = read_file(f1)
+    lines2 = read_file(f2)
     for i in range(len(lines1)):
         diff = True
         for j in range(len(lines2)):
             if lines1[i] == lines2[j]:
                 diff = False
         if diff:
-            print(sys.argv[1] + ',#' + str(i) + ': ' + lines1[i] + '\n' + sys.argv[2] + ',#' + str(i) + ': ' + lines2[i] + '\n')
+            print(f1 + ',#' + str(i) + ': ' + lines1[i] + '\n' + f2 + ',#' + str(i) + ': ' + lines2[i] + '\n')
+
+
+if len(sys.argv) == 3:
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+    compare(file1, file2)
+    compare(file2, file1)
